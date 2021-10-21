0e1883389f2d600b3aed327fe9daaf08cce11d40
Erik Mota Galindo <kenshimota@gmail.com>
2021-10-21 01:56:59 -0400

    view create map

---
+++ src/views/Map.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div ref="node-map" style="width: 100%;height: 600px;"></div>
+  </div>
+</template>
+
+<script>
+import HereMap from '../utils/here';
+export default {
+  data: () => ({ nodeMap: HereMap() }),
+
+  mounted: function() {
+    const nodeMap = this.nodeMap;
+    const nodeElement = this.$refs['node-map'];
+
+    nodeMap.createMap(nodeElement, {
+      zoom: 12,
+      center: { lng: -88.87, lat: 13.71 },
+    });
+
+    nodeMap.addBuble({ lng: -88.87, lat: 13.71 }, { content: '<span>Testing Erik</span>' });
+
+    console.log(nodeMap);
+  },
+};
+</script>
