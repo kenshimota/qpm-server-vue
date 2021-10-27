2d67482752ce6d266a073d02e8ad8ab27ec62f5f
Erik Mota Galindo <kenshimota@gmail.com>
2021-10-27 15:03:19 -0400

    add components list customers

--- src/components/customers/Index.vue
+++ src/components/customers/Index.vue
@@ -1,9 +1,14 @@
 <template>
-    <div></div>
+    <div>
+        <list-customers :language = "language" />
+    </div>
 </template>
 
 <script>
+import language from '../../languages/index';
+import ListCustomers from "./ListCustomers";
 export default {
-    
+    data: () => ({ language: language().content }),
+    components: { ListCustomers },
 }
 </script>
