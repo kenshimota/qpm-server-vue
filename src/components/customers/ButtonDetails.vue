5f79589f29630e3e34ed4e5a291d43446a5526d2
Erik Mota Galindo <kenshimota@gmail.com>
2021-10-27 23:27:15 -0400

    create component button the details of customers

---
+++ src/components/customers/ButtonDetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-button
+        @click = "openDetails"
+        outlined
+        type="is-info"
+        icon-right="map"
+        class="is-rounded"
+    />
+</template>
+
+<script>
+import DetailsCustomer from "./DetailsCustomer";
+export default {
+    props: ["customer", "reload", "language"],
+    methods: {
+        openDetails: function(){
+            this.$buefy.modal.open({
+                parent: this,
+                props: {
+                    customer: this.customer,
+                    language: this.language,
+                    reload: this.getData,
+                },
+                component: DetailsCustomer,
+                hasModalCard: true,
+                customClass: 'custom-class custom-class-2',
+                trapFocus: true,
+            });  
+        },
+    }
+
+}
+</script>
