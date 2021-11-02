5647efc2bfb5961dd94fda5f8985ec4d878e7ccf
Erik Mota Galindo <kenshimota@gmail.com>
2021-11-02 12:45:36 -0400

    remove handleSearch and add props reload

--- src/components/customers/ListCustomers.vue
+++ src/components/customers/ListCustomers.vue
@@ -29,15 +29,20 @@
                     {{ showField(props.row, "site_city") }}
                 </b-table-column>
                 <b-table-column :label = "language.ACTIONS" v-slot="props">
+                    <button-edit-customer 
+                        :language = "language"
+                        :customer = "props.row"
+                        :reload = "reload"
+                    />
                     <button-details 
                         :language = "language" 
                         :customer = "props.row" 
-                        :reload = "handleSearch"
+                        :reload = "reload"
                     />
                     <dialog-remove 
                         :language = "language" 
                         :customer = "props.row" 
-                        :reload = "handleSearch" 
+                        :reload = "reload" 
                     />
                 </b-table-column>
             </b-table>
@@ -46,32 +51,17 @@
 </template>
 
 <script>
-import { ClientQPM } from '../../utils/qpm';
 import DialogRemove from "./DialogRemove";
-import ButtonDetails from "./ButtonDetails"
+import ButtonDetails from "./ButtonDetails";
+import ButtonEditCustomer from "./ButtonEditCustomer";
 
 export default {
-    props: ["language", "snippet"],
-    components: { DialogRemove, ButtonDetails },
+    props: ["customers", "loading", "reload", "language", "snippet"],
+    components: { DialogRemove, ButtonDetails, ButtonEditCustomer },
 
     data: () => ({ 
-        customers: [], 
-        loading: false,
         currentPage: 1,
-        timeExec: null
     }),
-
-    watch: {
-        snippet: function(){
-            if(this.timeExec)
-                clearTimeout(this.timeExec);
-            this.timeExec = setTimeout(() => this.handleSearch(), 1000);
-        },
-    },
-
-    created: function(){
-        this.handleSearch();
-    },
 
     methods: {
 
@@ -80,23 +70,6 @@ export default {
             const sites = resource.sites;
             return (sites && sites[0] && sites[0][key]) || ""
         },
-
-        // function request to server for get customers
-        handleSearch: async function(){
-            try {
-                const snippet = this.snippet;
-                ClientQPM.method("searchCustomers", { searchstring: { snippet } });
-            
-                this.loading = true;
-                const customers = await ClientQPM.fetch().then(response => response.customers);
-
-                this.customers = customers;
-                this.loading = false;
-                this.currentPage = 1;
-            } catch(error){
-                console.error(error);
-            }
-        }
     }
 }
 </script>
