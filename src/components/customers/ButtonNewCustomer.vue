c80ac0b68a3e408892dc3ef4c39c553f4fe79bf5
Erik Mota Galindo <kenshimota@gmail.com>
2021-11-02 12:48:40 -0400

    creating component button new customer

---
+++ src/components/customers/ButtonNewCustomer.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-button
+        @click = "openDetails"
+        outlined
+        type="is-info"
+        icon-right="plus"
+        class = "is-rounded"
+        style = "float: left;"
+    />
+</template>
+
+<script>
+import DialogEditCustomer from "./DialogEditCustomer";
+export default {
+    props: ["reload", "language"],
+    methods: {
+        openDetails: function(){
+            this.$buefy.modal.open({
+                parent: this,
+                props: {
+                    language: this.language,
+                    reload: this.reload,
+                },
+                component: DialogEditCustomer,
+                hasModalCard: true,
+                customClass: 'custom-class custom-class-2',
+                trapFocus: true,
+            });  
+        },
+    }
+
+}
+</script>
