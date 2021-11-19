c8720b69706bd62a7371afb0fb2cada63c9293bc
Erik Mota Galindo <kenshimota@gmail.com>
2021-11-19 08:39:41 -0400

    correcting render graph

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref = "node-graph" style = "width: 100%; height: 600px">
+    <div ref = "node-graph" style = "width: 100%; height: 500px">
     </div>
 </template>
 
@@ -8,8 +8,6 @@ import WebGl from 'webgl-show';
 import sleep from "../utils/sleep";
 import WarehouseWithPallets from "../components-opengl/WarehouseWithPallets";
 export default {
-
-    data: () => ({ graph: null }),
     
     mounted: async function(){
         let element = this.$refs['node-graph'];
@@ -23,21 +21,15 @@ export default {
 
     methods: {
         
-        // crean el grafo
-        createGraph: async function(element) {
-            if(this.graph) delete this.graph;
+        makeScene: async function(element){            
             const graph = await WebGl({
                 element,
                 width: element.offsetWidth,
                 height: element.offsetHeight,
-                color: 'transparent',
+                color: 0xFFFFFF,
+                logs: true
             });
-            this.graph = graph;
-        },
-        
-        makeScene: async function(element){
-            await this.createGraph(element);
-            const graph = this.graph;
+
             const resource = new WarehouseWithPallets(graph);
 
             graph.addFigure( resource.render() );
@@ -45,11 +37,12 @@ export default {
             camera.position.z = 500;
             camera.position.y = 500;
             camera.position.x = 0;
-            camera.lookAt( graph._scene.position );
+            camera.lookAt( resource.render().figure.position );
             
             const { OrbitControls } = require('three/examples/jsm/controls/OrbitControls');
             const controls = new OrbitControls(camera, element);
             controls.update();
+
         },
     }
 
